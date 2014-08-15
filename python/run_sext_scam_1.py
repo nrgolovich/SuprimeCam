d6a4996ec07f259eb6cd35da06ec5c2de8d1a86f
cdfassnacht <cdfassnacht@ucdavis.edu>
2014-08-15 15:20:24 -0700

    Started implementing slight change of order in reduction steps

--- python/run_sext_scam_1.py
+++ python/run_sext_scam_1.py
@@ -2,10 +2,11 @@ import astromatic as ast
 import sys
 
 infile = sys.argv[1]
+whtfile = infile.replace('.fits','_wht_init.fits')
 regfile = infile.replace('.fits','.reg')
 outcat = infile.replace('.fits','.cat')
 print 'Running SExtractor on %s' % infile
 
-ast.make_cat_suprimecam(infile,outcat=outcat,regfile=regfile,
+ast.make_cat_suprimecam(infile,outcat=outcat,regfile=regfile,weight_file=whtfile,
                         configfile='sext_scam_astrom.config',satur=60000.)
 
