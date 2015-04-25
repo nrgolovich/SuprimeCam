8f2806345f0d5fd186e5ec6b18b5f3c0f921ec3c
wadawson <wadawson@ucdavis.edu>
2015-04-24 21:53:27 -0700

    modified make_tex_map.py  to be generic

--- python/make_texp_map.py
+++ python/make_texp_map.py
@@ -15,46 +15,44 @@ Example for the observations of the 1608+656 field in the r-band where each
 
 import glob
 import suprimecam_redux as scam
+import ccdredux
 import sys
 import os
 
 print len(sys.argv)
 
-if len(sys.argv)<4:
+if len(sys.argv)<2:
     print ""
-    print "ERROR: make_texp_map.py requires three input parameters"
-    print "  1. Root name for field"
-    print "  2. Filter used for observations"
-    print "  3. Exposure time in seconds for each of the input files"
+    print "ERROR: make_texp_map.py requires one input parameters"
+    print "  1. Exposure time in seconds for each of the input files"
     print ""
-    print "For example, for observations of the 1608+656 field in the r-band"
-    print "  where each input exposure is 300 seconds, type"
-    print ""
-    print "   python make_texp_map.py 1608 r 300"
+    print "For example, "
+    print "   python make_texp_map.py 300"
     print ""
     exit()
 
 infiles = glob.glob('object*resamp_wht.fits')
-rootname = sys.argv[1]
-obsband = sys.argv[2]
-texp = float(sys.argv[3])
+#rootname = sys.argv[1]
+#obsband = sys.argv[1]
+texp = float(sys.argv[2])
 
 print ""
 print "Making the exposure maps for the individual input files"
 print "-------------------------------------------------------"
-scam.make_texp_map(infiles,texp)
+ccdredux.make_texp_map(infiles,texp)
 
 print ""
 print "Using swarp to combine the individual files into the final exposure map"
 print "-----------------------------------------------------------------------"
-configfile = 'swarp_%s_texp.config' % rootname
+#configfile = 'swarp_%s_texp.config' % rootname
+configfile = 'swarp_texp.config'
 if os.path.isfile(configfile) is False:
     print ""
     print 'ERROR: Missing the expected swarp configuration file: %s' % configfile
     print ''
     exit()
 else:
-    outfile = '%s_scam_mar14_%s_texp.fits' % (rootname,obsband)
+    outfile = 'scam_texp.fits'
     os.system('swarp *texp.fits -c %s -IMAGEOUT_NAME %s' % (configfile,outfile))
     print ''
     print 'Created output exposure time map: %s' % outfile
