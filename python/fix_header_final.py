be0af913a91c3a732280e32a04f784f2efa7aee4
Will Dawson <will@dawsonresearch.com>
2015-04-25 17:07:11 -0700

    corrected fix_header_final.py typo

--- python/fix_header_final.py
+++ python/fix_header_final.py
@@ -3,7 +3,7 @@ import sys
 '''
 Corrects the header information regarding zeropoint and exposure time.
 '''
-scifile = sys.arg[1]
+scifile = sys.argv[1]
 zeropt = float(sys.argv[2])
 texpfile = scifile.replace('.fits','_texp.fits')
 scam.fix_final_headers(scifile,texpfile,zeropt)
